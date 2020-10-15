0479c7d1e9ab81a0bc9d3268bf1ce4fcecd3e698
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2020-10-15 17:05:54 +0200

    Fix retrieve vip
    Signed-off-by: delphes99 <mail@github.laurent-gautho-lapeyre.fr>

--- src/main/kotlin/fr/delphes/bot/event/outgoing/RetrieveVip.kt
+++ src/main/kotlin/fr/delphes/bot/event/outgoing/RetrieveVip.kt
@@ -4,6 +4,6 @@ import com.github.twitch4j.chat.TwitchChat
 
 object RetrieveVip : OutgoingEvent {
     override fun executeOnTwitch(chat: TwitchChat, ownerChat: TwitchChat, channel: String) {
-        chat.sendMessage(channel, "/vips")
+        ownerChat.sendMessage(channel, "/vips")
     }
 }
