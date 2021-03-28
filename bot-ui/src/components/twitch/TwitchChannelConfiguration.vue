75abfe7f75a540734bff92b8d7858cac7f39ff8c
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2021-03-28 11:31:25 +0200

    Bot UI : Twitch Channel configuration tab
    Signed-off-by: delphes99 <mail@github.laurent-gautho-lapeyre.fr>

--- bot-ui/src/components/twitch/TwitchChannelConfiguration.vue
+++ bot-ui/src/components/twitch/TwitchChannelConfiguration.vue
@@ -1,6 +1,16 @@
 <template>
-  <h1>{{channelName}}</h1>
-  <router-view></router-view>
+  <h1>{{ channelName }}</h1>
+  <div class="w-full flex flex-row">
+    <router-link :to="`/twitch/${channelName}/rewards`" class="link" active-class="active-link">
+      Rewards
+    </router-link>
+    <router-link :to="`/twitch/${channelName}/features`" class="link" active-class="active-link">
+      Features
+    </router-link>
+  </div>
+  <div class="p-4 bg-gray-300 bg-black">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,3 +20,13 @@ export default {
   props: ['channelName']
 }
 </script>
+
+<style lang="css" scoped>
+  .link {
+    @apply mr-2 px-3 bg-gray-300 text-center
+  }
+
+  .active-link {
+    @apply bg-gray-500
+  }
+</style>
