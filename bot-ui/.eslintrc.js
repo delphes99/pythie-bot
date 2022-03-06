504e61adca1a3825e699bfd491e005b9b2d49293
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2022-03-06 15:33:29 +0100

    Bot-ui : upgrade vue version
    Signed-off-by: delphes99 <mail@github.laurent-gautho-lapeyre.fr>

--- bot-ui/.eslintrc.js
+++ bot-ui/.eslintrc.js
@@ -1,20 +1,1 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
+module.exports = {  root: true,  env: {    node: true,    "vue/setup-compiler-macros": true  },  extends: [    "plugin:vue/vue3-recommended",    "eslint:recommended",    "@vue/typescript/recommended",    "@vue/prettier",    "@vue/prettier/@typescript-eslint"  ],  parser: "vue-eslint-parser",  parserOptions: {    parser: "@typescript-eslint/parser",    sourceType: "module",    ecmaVersion: 2020  },  rules: {    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",    "vue/script-setup-uses-vars": "error"  }};
