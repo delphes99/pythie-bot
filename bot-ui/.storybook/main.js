720250b8c8881cc27114ee36fc4773a167bb0092
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2022-05-05 00:48:15 +0200

    Bot-ui : Storybook setup
    Signed-off-by: delphes99 <mail@github.laurent-gautho-lapeyre.fr>

---
+++ bot-ui/.storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-tailwind-dark-mode",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: "@storybook/vue3",
+}
