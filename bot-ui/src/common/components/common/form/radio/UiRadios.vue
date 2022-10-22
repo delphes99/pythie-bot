656ed78267fa642dcc409eda2111e24d05572dd2
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2022-10-22 20:50:27 +0200

    Bot-ui : Better radio buttons
    Signed-off-by: delphes99 <mail@github.laurent-gautho-lapeyre.fr>

--- bot-ui/src/common/components/common/form/radio/UiRadios.vue
+++ bot-ui/src/common/components/common/form/radio/UiRadios.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="radio-button-group">
+  <div>
     <label
       v-if="title"
     >{{ title }}</label>
-    <span
-      v-for="option in options.values"
-      :key="option.id"
-    >
-      <input
-        :id="option.id"
-        type="radio"
-        :name="name"
-        :value="option.value"
-        :checked="modelValue === option.value"
-        class="inline"
-        @change="$emit('update:modelValue', option.value)"
+    <div class="flex items-center h-">
+      <span
+        v-for="option in options.values"
+        :key="option.id"
       >
-      <label
-        :for="option.id"
-        class="inline pl-2"
-      >
-        {{ option.label() }}
-      </label>
-    </span>
+        <input
+          :id="option.id"
+          type="radio"
+          :name="name"
+          :value="option.value"
+          :checked="modelValue === option.value"
+          class="inline h-4 w-4"
+          @change="$emit('update:modelValue', option.value)"
+        >
+        <label
+          :for="option.id"
+          class="inline p-3 text-base font-medium"
+        >
+          {{ option.label() }}
+        </label>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -50,5 +52,5 @@ defineProps({
   },
 })
 
-defineEmits(['update:modelValue'])
+defineEmits(["update:modelValue"])
 </script>
