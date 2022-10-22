a3cff8830d3cf8da963a657b6a09734512701fa3
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2022-10-22 18:36:36 +0200

    Bot-ui & Bot-overlay : Don't rebuild npm apps if no change
    Signed-off-by: delphes99 <mail@github.laurent-gautho-lapeyre.fr>

--- bot-ui/build.gradle.kts
+++ bot-ui/build.gradle.kts
@@ -10,6 +10,15 @@ val buildTaskUsingYarn = tasks.register<NpmTask>("buildNpm") {
     dependsOn(tasks["npmInstall"])
     dependsOn(NpmInstallTask.NAME)
 
+    inputs.files(fileTree("node_modules"))
+    inputs.files(fileTree("src"))
+    inputs.files(fileTree("public"))
+    inputs.file("package.json")
+    inputs.file("tailwind.config.js")
+    inputs.file("index.html")
+
+    outputs.dir("dist")
+
     npmCommand.set(listOf("run", "build"))
 }
 
