00c6267f416ba24168ab040b8dba6af459a50a2e
delphes99 <mail@github.laurent-gautho-lapeyre.fr>
2023-04-10 21:20:35 +0200

    Twitch-Connector : Fix incoming message

--- connector-twitch/src/main/kotlin/fr/delphes/connector/twitch/irc/TwitchIrcConnectionManager.kt
+++ connector-twitch/src/main/kotlin/fr/delphes/connector/twitch/irc/TwitchIrcConnectionManager.kt
@@ -6,9 +6,11 @@ import fr.delphes.bot.connector.connectionstate.ConnectionSuccessful
 import fr.delphes.bot.event.outgoing.OutgoingEvent
 import fr.delphes.connector.twitch.TwitchConfiguration
 import fr.delphes.connector.twitch.TwitchConnector
+import fr.delphes.connector.twitch.eventMapper.ChannelMessageMapper
 import fr.delphes.connector.twitch.outgoingEvent.TwitchChatOutgoingEvent
 import fr.delphes.twitch.irc.IrcChannel
 import fr.delphes.twitch.irc.IrcClient
+import kotlinx.coroutines.runBlocking
 
 class TwitchIrcConnectionManager(
     private val connector: TwitchConnector,
@@ -20,7 +22,15 @@ class TwitchIrcConnectionManager(
     override suspend fun doConnection(configuration: TwitchConfiguration): ConnectionSuccessful<TwitchConfiguration, TwitchIrcRuntime> {
         val credentialsManager = connector.configurationManager.buildCredentialsManager() ?: error("Connection with no configuration")
 
-        val ircClient = IrcClient.builder(configuration.botIdentity?.channel!!, credentialsManager).build()
+        val ircClient = IrcClient.builder(configuration.botIdentity?.channel!!, credentialsManager)
+            .withOnChannelMessage { message ->
+                runBlocking {
+                    ChannelMessageMapper(message.channel.toTwitchChannel(), connector)
+                        .handle(message)
+                        .forEach { connector.bot.handle(it) }
+                }
+            }
+            .build()
         ircClient.connect()
 
         configuration.listenedChannels.forEach { channel ->
